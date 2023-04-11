93d6ef621df034adf8adc0b3e9fea418656520cd
ryan2023 <79850835+ryan2023@users.noreply.github.com>
2023-04-11 16:53:16 +0100

    Add files via upload

---
+++ main_gtz - Copy.c
@@ -0,0 +1,164 @@
+/*
+ *  ======== gtz.c ========
+ */
+
+#include <xdc/std.h>
+#include <xdc/runtime/System.h>
+
+#include <ti/sysbios/BIOS.h>
+#include <ti/sysbios/knl/Clock.h>
+#include <ti/sysbios/knl/Task.h>
+
+#include <xdc/runtime/Types.h>
+#include <xdc/runtime/Timestamp.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include "gtz.h"
+
+void clk_SWI_Read_Data(UArg arg0);
+void clk_SWI_GTZ_All_Freq(UArg arg0);
+
+extern void task0_dtmfGen(void);
+extern void task1_dtmfDetect(void);
+
+extern int sample, tdiff, tdiff_final, gtz_out[8];
+extern short coef[8];
+extern int flag;
+
+short data[NO_OF_SAMPLES];
+short *buffer;
+
+/*
+ *  ======== main ========
+ */
+int main() {
+	System_printf("\n System Start\n");
+	System_flush();
+
+	/* Read binary data file */
+	FILE* fp = fopen("../data.bin", "rb");
+	if(fp==0) {
+		System_printf("Error: Data file not found\n");
+		System_flush();
+		return 1;
+	}
+	fread(data, 2, NO_OF_SAMPLES, fp);
+	buffer = (short*)malloc(2*8*10000);
+
+	/* Create a Clock Instance */
+	Clock_Params clkParams;
+
+	/* Initialise Clock Instance with time period and timeout (system ticks) */
+	Clock_Params_init(&clkParams);
+	clkParams.period = 1;
+	clkParams.startFlag = TRUE;
+
+	/* Instantiate ISR for tone generation  */
+	Clock_create(clk_SWI_Read_Data, TIMEOUT, &clkParams, NULL);
+
+	/* Instantiate 8 parallel ISRs for each of the eight Goertzel coefficients */
+	Clock_create(clk_SWI_GTZ_All_Freq, TIMEOUT, &clkParams, NULL);
+
+	/* Start SYS_BIOS */
+	BIOS_start();
+}
+
+/*
+ *  ====== clk_SWI_Generate_DTMF =====
+ *  Dual-Tone Generation
+ *  ==================================
+ */
+void clk_SWI_Read_Data(UArg arg0) {
+	static int tick;
+	tick = Clock_getTicks();
+	sample = data[tick%NO_OF_SAMPLES];
+}
+
+/*
+ *  ====== clk_SWI_GTZ =====
+ *  gtz sweep
+ *  ========================
+ */
+void clk_SWI_GTZ_All_Freq(UArg arg0) {
+	// define variables for timing
+	static int start, stop;
+
+	// define feedback times as N
+	static int N = 0;
+	static int k;
+	static int j;
+	static int q;
+	static int Q0;
+	static int Q1[8] = {0};
+	static int Q2[8] = {0};
+	static int Q1_square;
+	static int Q2_square;
+	static int Q1Q2_coef;
+	short input;
+   	//Record start time
+	start = Timestamp_get32();
+
+	static int Goertzel_Value = 0;
+	input =  (sample);
+	// input = input >> 12;
+
+	/* TODO 1. Complete the feedback loop of the GTZ algorithm*/
+	/* ========================= */
+
+	for(k=0; k<8; k++)
+	{
+
+
+		Q0 = (coef[k] * Q1[k] >> 14) - Q2[k] + input;
+		Q2[k] = Q1[k];
+		Q1[k] = Q0;
+
+
+
+
+
+	/* ========================= */
+
+
+	//Record stop time
+	stop = Timestamp_get32();
+	//Record elapsed time
+	tdiff = stop-start;
+
+	if(N == 206){
+	   	//Record start time
+		start = Timestamp_get32();
+
+		/* TODO 2. Complete the feedforward loop of the GTZ algorithm*/
+		/* ========================= */
+		for(j=0; j<8; j++){
+		     Q1_square = (Q1[j]*Q1[j]);
+		     Q2_square = (Q2[j]*Q2[j]);
+		     Q1Q2_coef = (Q1[j]*Q2[j]*(coef[j] >> 14));
+		     Goertzel_Value = Q1_square + Q2_square - Q1Q2_coef;
+
+		     gtz_out[j] = sqrt(Goertzel_Value);
+		}
+
+		/* ========================= */
+		flag = 1;
+		for (q=0;q<8;q++)
+		{
+			Q1[q] = 0;
+			Q2[q] = 0;
+		}
+		N = 0;
+
+		//Record stop time
+		stop = Timestamp_get32();
+		//Record elapsed time
+		tdiff_final = stop-start;
+
+	}
+	}
+	N++;
+
+}
